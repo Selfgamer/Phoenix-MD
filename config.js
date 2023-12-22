a5fb33430b9daf7f2e92dc2c18c879e42221772a
Selfgamer <146574920+Selfgamer@users.noreply.github.com>
2023-12-22 10:03:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
 let HANDLER = "false";
 
 module.exports = {
-  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  ANTILINK: toBool(process.env.ANTI_LINK) || true,
   
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
